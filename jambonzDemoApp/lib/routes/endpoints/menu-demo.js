0d1c686e394554a37dac4c47b9f273b5ce3271b2
codingmarisa <codingmarisa@gmail.com>
2022-11-04 22:10:56 -0700

    add logs

--- jambonzDemoApp/lib/routes/endpoints/menu-demo.js
+++ jambonzDemoApp/lib/routes/endpoints/menu-demo.js
@@ -4,6 +4,28 @@ const WebhookResponse = require('@jambonz/node-client').WebhookResponse;
 // User calls a fixed phone number with this app set to the number in the Phone Numbers section of the web portal
 // Phone number is a Twilio phone number
 
+// Does this webhook cause an error that restart feature-server?
+router.post('/answeringMachineDetection', async (req, res) => {
+    const { logger } = req.app.locals;
+    const payload = req.body;
+    const { type, reason } = payload;
+    let currentTime = new Date().getTime();
+    logger.info({payload}, currentTime + ' POST /menu-demo/menu-greeting');
+    console.log(`/answeringMachineDetection type: ${type}, reason: ${reason}`);
+
+    if (type === 'amd_no_speech_detected') {
+      return {
+        "verb": "say",
+        "text": "Record your message and press pound or press star to contact the operator.",
+        "synthesizer" : {
+          "vendor": "Google",
+          "language": "en-US"
+        }
+      }
+    }
+    //res.sendStatus(200); // this would return before line 16. Is a return needed if amd_no_speech_detected does not trigger?
+  });
+
 const speechGreeting = `<speak>
 <s>This is a demo app. Please input or say the extension of the employee you are trying to reach.</s>
 <break time="5s"/>
@@ -93,26 +115,5 @@ router.post('/process-selection-greeting', async (req, res) => {
         res.sendStatus(503);
     }
 });
-
-router.post('/answeringMachineDetection', async (req, res) => {
-    const { logger } = req.app.locals;
-    const payload = req.body;
-    const { type, reason } = payload;
-    let currentTime = new Date().getTime();
-    logger.info({payload}, currentTime + ' POST /menu-demo/menu-greeting');
-    console.log(`/answeringMachineDetection type: ${type}, reason: ${reason}`);
-
-    if (type === 'amd_no_speech_detected') {
-      return {
-        "verb": "say",
-        "text": "Record your message and press pound or press star to contact the operator.",
-        "synthesizer" : {
-          "vendor": "Google",
-          "language": "en-US"
-        }
-      }
-    }
-    res.sendStatus(200);
-  });
   
-  module.exports = router;
+module.exports = router;
